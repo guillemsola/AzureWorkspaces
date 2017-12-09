db7d95060d7cfdb297203b504e2334a93a429481
Guillem Sola <Guillem.Sola@asg.com>
2017-12-09 23:21:01 +0100

    Set fix db pwd

--- AzureWorkspacesDeployment/Artifacts/rabbit.sh
+++ AzureWorkspacesDeployment/Artifacts/rabbit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
-apt install rabbitmq-server -y
+apt-get update
+apt-get upgrade -y
+apt-get install rabbitmq-server -y
 rabbitmq-plugins enable rabbitmq_management
 rabbitmqctl add_user portal workspace2017!
 rabbitmqctl set_user_tags portal administrator
